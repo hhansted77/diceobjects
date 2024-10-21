cc9b5e38f56c22526ba3c89f2ca116ea50ffbd64
Hannah Hansted <hmh22@fsu.edu>
2024-10-20 23:28:28 -0400

    everytime we touch i get this feeling (homicide)

--- sketch.js
+++ sketch.js
@@ -69,7 +69,7 @@ function shakeDice() {
     const die = dice[i];
     die.roll();
     list = list + die.value + " ";
-    score = score + " ";
+    score = score + die.roll;
 
     if (score == 2550){
       win.play();
